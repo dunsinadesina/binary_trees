1e598d18e54913a2e16a3abe5566a91d815e0eef
dunsinadesina <adesinajesudunsin@gmail.com>
2024-02-27 05:19:57 -0800

    updated

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree)
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 	return (0);
 }
@@ -20,12 +20,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
-
 	if (tree)
 	{
-		l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		size_t l_height, r_height;
+
+		l_height = 0, r_height = 0;
+		l_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l_height > r_height) ? l_height : r_height);
 	}
 	return (0);
